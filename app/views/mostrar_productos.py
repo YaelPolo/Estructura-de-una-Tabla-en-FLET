e6a86effe0dd8720d7da96b63f833ca7d2a96195
“YaelPolo” <iamyaelpolo@gmail.com>
2026-03-13 12:31:31 -0600

    y

--- app/views/mostrar_productos.py
+++ app/views/mostrar_productos.py
@@ -44,4 +44,35 @@ def products_view(page: ft.page) -> ft.control:
                 data_row_min_height = 48,
         )
 
+        #return tabla
+
+        async def actualizar_data():
+                nonlocal rows_data, total_items
+                try:
+                        data = listar_productos(limit = 500, offset = 0) #Se conecta a transacciones_api_productos.py
+                        total_items = int(data.get("total", 0))
+                        #print(total_items)
+                        total_text.value = "Total de productos: " + str(total_items)
+                        rows_data = data.get("items", [] or [])
+                        actualizar_filas()
+                except Exception as ex:
+                        await show_snackbar(page, "Error" , str(ex) , bgcolor= Colors.DANGER) # type: ignore
+        
+        def actualizar_filas():
+                nuevas_filas = []
+                for p in rows_data:
+                        nuevas_filas.append(
+                                ft.DataRow(
+                                        cells = [
+                                                ft.DataCell(ft.Text(p.get("nombre", "N/A"))),
+                                                ft.DataCell(ft.Text(str(p.get("cantidad", "N/A")))),
+                                                ft.DataCell(ft.Text(p.get("fecha_ingreso", "N/A"))),
+                                                ft.DataCell(ft.Text(str(p.get("cantidad_minima", "N/A")))),
+                                                ft.DataCell(ft.Text(str(p.get("cantidad_maxima", "N/A")))),
+                                        ]
+                                )
+                        )
+                        tabla.rows = nuevas_filas
+                        page.update()
+        page.run_task(actualizar_data)
         return tabla
